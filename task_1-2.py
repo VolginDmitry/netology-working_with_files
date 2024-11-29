928759d29445db177fef61921af05bad0c249323
Dmitry-admin <volginda@gmail.com>
2024-11-29 20:29:40 +0400

    removed the extra lines

--- task_1-2.py
+++ task_1-2.py
@@ -1,13 +1,3 @@
-'''with open('recipes.txt') as f:
-     #data = f.readline()#.strip()
-     for line in f:
-         ingredient_name = line.strip()
-         ingredient_count = f.readline().strip()
-         #print(ingredient_name)
-         print(int(ingredient_count))'''
-         
-
-
 with open('recipes.txt') as f:
     cook_book = {}
     for line in f:
@@ -42,36 +32,3 @@ def get_shop_list_by_dishes(dishes, person_count):
         return cooking_list
 
 print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
-         
-         
-#print('   skjfn   \n sfvsv'.strip())
-
-
-"""
-def parse_cookbook(file_path):
-    cook_book = {}
-    
-    with open(file_path, 'r', encoding='utf-8') as file:
-        while True:
-            dish_name = file.readline().strip()  # Читаем название блюда
-            if not dish_name:  # Если достигнут конец файла
-                break
-            
-            # Читаем количество ингредиентов
-            ingredients_count = int(file.readline().strip())
-            ingredients = []
-
-            for _ in range(ingredients_count):
-                line = file.readline().strip()
-                ingredient_name, quantity, measure = line.split(' | ')
-                ingredient = {
-                    'ingredient_name': ingredient_name,
-                    'quantity': int(quantity),
-                    'measure': measure
-                }
-                ingredients.append(ingredient)
-
-            # Добавляем информацию о блюде в словарь
-            cook_book[dish_name] = ingredients
-    
-    return cook_book"""
